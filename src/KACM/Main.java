b0aea5c724871db673e56952f73865d63eb625c6
Marrakchino <nabilelqatib@gmail.com>
2016-12-26 12:12:30 +0100

    Update Main.java

--- src/KACM/Main.java
+++ src/KACM/Main.java
@@ -1,4 +1,4 @@
-package KACM;
+package nextfixturesms;
 
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
@@ -6,27 +6,26 @@ import com.twilio.type.PhoneNumber;
 
 public class Main {
 
-	public static final String ACCOUNT_SID = "AC93ebe79dcc74c372382e1333964d229b";
-	public static final String AUTH_TOKEN = "df7b1fb35579093f2bf916fba57c311c";
+	public static final String ACCOUNT_SID = "<put your account sid here>";
+	public static final String AUTH_TOKEN = "<put your authentificatio token here>";
 
 	public static void main(String[] args) {
 		Fixture nextFixture = Parser.getNextFixture();
 	    String messageText = prepareMessage(nextFixture);
 	    		
 	    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-	    Message message = Message.creator(new PhoneNumber("+33664444309"),
-	        new PhoneNumber("+33644600250"), 
+	    Message message = Message.creator(new PhoneNumber(""),
+	        new PhoneNumber(""), // change these with your personal phone number and twilio's number
 	        messageText).create();
 		System.out.println(message.getSid());
 	}
 
+	// change the sent text message as you want it to be
 	public static String prepareMessage(Fixture nextFixture)
 	{
-	    String messageText = new String("Le prochain match du KACM se jouera le ");
+	    String messageText = new String("");
 	    messageText += nextFixture.getDate().toString();
-	    messageText += " à ";
 	    messageText += nextFixture.getTime();
-	    messageText += " contre ";
 	    messageText += nextFixture.getOpponent();
 	    return messageText;
 	}
