f96ba456bafc510263df4c5e079a0cc52edbd036
Nabil Elqatib <nabilelqatib@gmail.com>
2017-04-22 21:16:52 +0200

    Fix #1
    Add methods in Fixture to check if a fixture has already been played and
    doing so iterate over the list of parsed fixtures until we get the most
    imminent one.

--- src/nextfixturesms/Fixture.java
+++ src/nextfixturesms/Fixture.java
@@ -10,7 +10,7 @@ public class Fixture {
 	private String opponent;
 	private String time;
 	public String[] POSSIBLE_OPPONENTS = {""};
-	
+
 	public LocalDate getDate() {
 		return date;
 	}
@@ -26,25 +26,25 @@ public class Fixture {
 	private void setOpponent(String opponent) {
 		this.opponent = opponent;
 	}
-	
+
 	private void setLocalDate(LocalDate date)
 	{
 		this.date = date;
 	}
-	
-	
+
+
 	public Fixture(String time, String date, String teams)
 	{
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy", Locale.FRANCE); // e.g: 30.12.16
 		LocalDate fixtureDate = LocalDate.parse(date, formatter);
 		this.setLocalDate(fixtureDate);
-		
+
 		String opponent = this.parseOpponent(teams);
 		this.setOpponent(opponent);
-		
+
 		this.time = time;	
 	}
-	
+
 	private String parseOpponent(String teams)
 	{
 		String possibleOpponent;
@@ -58,7 +58,7 @@ public class Fixture {
 		}
 		return possibleOpponent;
 	}
-	
+
 	private boolean isAPossibleOpponent(String opponent)
 	{
 		for (String possibleOpponent : POSSIBLE_OPPONENTS)
@@ -68,8 +68,60 @@ public class Fixture {
 		}
 		return false;
 	}
-	
+
 	public Fixture()
 	{
 	}
-}
+
+
+	public boolean isNotYetPlayed(){
+		Calendar cal = Calendar.getInstance();
+		int hourOfDay = cal.get(Calendar.HOUR_OF_DAY);
+		int minOfHour = cal.get(Calendar.MINUTE);
+		int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+		int monthOfYear = cal.get(Calendar.MONTH) + 1;
+		int year = cal.get(Calendar.YEAR);
+
+		int fixtureDay = this.getDate().getDayOfMonth();
+		int fixtureMonth = this.getDate().getMonthValue();
+		// int fixtureYear = this.getDate().getYear();
+
+		int fixtureHour = Integer.parseInt(this.getTime().split(":")[0]);
+		int fixtureMin = Integer.parseInt(this.getTime().split(":")[1]);
+
+		if (fixtureMonth < monthOfYear){
+			return false;
+		}
+		else if (fixtureMonth > monthOfYear) {
+			return true;
+		}
+		else {
+			if (fixtureDay < dayOfMonth) {
+				return false;
+			}
+			else if (fixtureDay > dayOfMonth) {
+				return true;
+			}
+			else {  meme jour
+				if (fixtureHour < hourOfDay){
+					return false;
+				}
+				else {
+					if (fixtureMin > minOfHour){
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Fixture [date=" + date + ", opponent=" + opponent + ", time=" + time + ", atHome=" + atHome + "]";
+	}
--- src/nextfixturesms/Parser.java
+++ src/nextfixturesms/Parser.java
@@ -9,9 +9,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Parser {
- 	private static final String SOURCE_URL = "http://www.matchendirect.fr/equipe/*****.html"; 
+	private static final String SOURCE_URL = "http://www.matchendirect.fr/equipe/*****.html"; 
 	// change this with your team's html page on this website
-	
+
 	public static Fixture getNextFixture()
 	{
 		Fixture nextFixture;
@@ -19,20 +19,42 @@ public class Parser {
 		try {
 			doc = Jsoup.connect(SOURCE_URL).get();
 			Elements matchGridElements = doc.getElementsByClass("tableau");
-			
-			Iterator<Element> usefulElementsIterator = matchGridElements.first().select("td").iterator();
-			String fixtureTime = usefulElementsIterator.next().text();
-			String fixtureDate = usefulElementsIterator.next().text();
-			String fixtureTeams = usefulElementsIterator.next().text();
-			
-			nextFixture = new Fixture(fixtureTime, fixtureDate, fixtureTeams);
+
+			SimpleDateFormat sdfDate = new SimpleDateFormat("dd.MM.yy");
+			Date today = Calendar.getInstance().getTime(); 
+
+			List<Fixture> allFixtures = new ArrayList<Fixture>();
+			String fixtureTime, fixtureDate, fixtureTeams;
+			for (int i = 0; i < matchGridElements.first().getElementsByClass("lm1").size(); i++){
+				fixtureTime = matchGridElements.first().getElementsByClass("lm1").get(i).text();
+				fixtureDate = matchGridElements.first().getElementsByClass("lm2_0").get(i).text();
+				if (fixtureDate.equals("Aujourd'hui")){ // J'espere qu'il y a pas de demain...
+					fixtureDate = sdfDate.format(today);
+				}
+				fixtureTeams = matchGridElements.first().getElementsByClass("lm3").get(i).text();
+				allFixtures.add(new Fixture(fixtureTime, fixtureDate, fixtureTeams));
+			}
+
+			nextFixture = allFixtures.get(0);
+			int i = 1;
+			while (nextFixture.isNotYetPlayed()){
+				nextFixture = allFixtures.get(i);
+				i++;
+			}
+
 			return nextFixture;
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 		System.out.println("No fixture found!");
-		return null;
+		return null; // ...
+
 	}
+}
+}
+}
+}
 
 }
